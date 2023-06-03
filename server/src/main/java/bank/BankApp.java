d7694aa924e5cd28f11e2df004a4f274d66925a3
filipmilo <filip.milosevic29@gmail.com>
2023-06-03 17:44:47 +0200

    Initial commit (Add project skeleton)

---
+++ server/src/main/java/bank/BankApp.java
@@ -0,0 +1,49 @@
+package bank;
+import lombok.AllArgsConstructor;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieScanner;
+import org.kie.api.runtime.KieContainer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+
+@SpringBootApplication
+@EntityScan("demo.facts")
+@AllArgsConstructor
+public class BankApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApp.class, args);
+	}
+
+	@Bean
+	public KieContainer kieContainer() {
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks
+				.newKieContainer(ks.newReleaseId("bank", "rules", "0.0.1-SNAPSHOT"));
+		KieScanner kScanner = ks.newKieScanner(kContainer);
+		kScanner.start(10_000);
+		return kContainer;
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+
+		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+}
---
+++ server/src/main/java/bank/controller/HelloController.java
@@ -0,0 +1,14 @@
+package bank.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping(value = "/hello", produces = "application/json")
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<>("Hello world!", HttpStatus.OK);
+    }
+}
