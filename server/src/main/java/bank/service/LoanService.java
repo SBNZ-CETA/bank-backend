bc61d0b2bc1c68f2e294a576672e4ff85837c0f0
filipmilo <filip.milosevic29@gmail.com>
2023-06-23 15:33:30 +0200

    Update loan query to feature backward chaining

--- server/src/main/java/bank/service/LoanService.java
+++ server/src/main/java/bank/service/LoanService.java
@@ -5,16 +5,35 @@ import dtos.LoanRequestDto;
 import facts.Loan;
 import facts.User;
 import lombok.AllArgsConstructor;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.QueryResults;
+import org.kie.api.runtime.rule.Variable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class LoanService {
-   public LoanRepository loanRepository;
-   public Loan processLoanRequest(LoanRequestDto request, User user)  {
-       //TODO: Import user and his transactions
-       //TODO: Make kie session and fire all rules
-       //TODO: Pull query result from session
-      return null;
+   private LoanRepository loanRepository;
+   private KieContainer kieContainer;
+
+   public List<Loan> processLoanRequest(LoanRequestDto request, User user)  {
+      KieSession kieSession = kieContainer.newKieSession();
+
+      loanRepository.findAll().forEach(kieSession::insert);
+      kieSession.insert(user);
+      kieSession.insert(request);
+      //kieSession.fireAllRules();
+
+      QueryResults result = kieSession.getQueryResults("findFittingLoan", new Object[]{Variable.v});
+
+      List<Loan> loansAvailable = new ArrayList<>();
+      result.forEach(loan -> loansAvailable.add(loanRepository.findById((Long)loan.get("$id")).get()));
+
+      kieSession.dispose();
+      return loansAvailable;
    }
 }
